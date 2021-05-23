265b6a9be7a24185641474aaaae16c77189ae551
Lartu <martu@LartuAir.local>
2021-05-23 00:50:34 -0300

    Upload Sun May 23 00:50:34 -03 2021

--- upload.sh
+++ upload.sh
@@ -1,6 +1,7 @@
 date >> changelog
 git diff --stat --relative source | grep -E ".*\.coso.*" >> changelog
 python3 coso.py --save-changelog
+sleep 1
 git add --all
 git commit -m "Upload $(date)"
 git push
