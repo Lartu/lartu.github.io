3ec99c38ef503753bd9991f70e016428af8c459d
Lartu <lartu@lartu.net>
2024-01-06 21:52:14 -0300

    Upload Sat Jan  6 21:52:14 -03 2024

--- changelog.sh
+++ changelog.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-echo ""
-echo ""
-echo "<h5>$(date)</h5>"
-echo "<p class=\"par changelog\">"
-echo "<i>" `git diff --stat HEAD HEAD~1 | tail -n 1` "</i><br>"
-status_list=`git diff --stat HEAD HEAD~1`
+status_list=$(git diff --stat HEAD HEAD~1)
 status_list="${status_list/docs\//}"
-echo "$status_list" | grep '.html' | awk 'ORS="<br>\n"' | sed 's/-\{1,\}/<span class="rem">--<span>/g; s/+\{1,\}/<span class="add">++<span>/g'
-echo "</p>"
+
+if echo "$status_list" | grep -q '.html'; then
+    echo ""
+    echo ""
+    echo "<h5>$(date)</h5>"
+    echo "<p class=\"par changelog\">"
+    # echo "<i>" `git diff --stat HEAD HEAD~1 | tail -n 1` "</i><br>"
+    echo "$status_list" | grep '.html' | awk 'ORS="<br>\n"' | sed 's/-\{1,\}/<span class=\"rem\">--<\/span>/g; s/+\{1,\}/<span class=\"add\">++<\/span>/g'
+    echo "</p>"
+fi
