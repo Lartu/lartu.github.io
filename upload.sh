f266cb4c41ab5d90d2687de17dcaa72bfea79469
Lartu <11744462+Lartu@users.noreply.github.com>
2024-08-31 02:00:00 -0300

    Upload Sat Aug 31 02:00:00 -03 2024

--- upload.sh
+++ upload.sh
@@ -6,7 +6,8 @@ mkdir docs
 mv *.html docs
 cp -r include docs
 cp -r images docs
+cp -r files docs  # EF!! PDR!!
 echo "lartu.net" > docs/CNAME
-#git add --all
-#git commit -m "Upload $(date)"
-#git push
+git add --all
+git commit -m "Upload $(date)"
+git push
