9a00a4b8991fac8cf0b182752d4e10fad86303e6
Lartu <lartu@lartu.net>
2025-02-24 00:35:08 -0300

    Upload Mon Feb 24 00:35:08 -03 2025

--- regenerate.sh
+++ regenerate.sh
@@ -1,7 +1,2 @@
 set -e
 kat lartunet.kat > index.html
-kat about.kat > about.html
-kat games.kat > games.html
-kat langs.kat > langs.html
-kat links.kat > links.html
-kat thoughts.kat > thoughts.html
